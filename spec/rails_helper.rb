75d112dc3f67e0c582f38fe1bb6a8c0a93a8aba0
kHigasa <kojihigasa@gmail.com>
2018-04-02 16:47:03 +0900

    Add specs to NavigationHelper's collapsible_links_partial_path method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,7 +57,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  onfig.before(:suite) do
+  config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
 
