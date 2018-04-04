6bb7ca70f5ba56675e8dbf55f9eaa1cdbed2dd07
kHigasa <kojihigasa@gmail.com>
2018-04-04 10:29:32 +0900

    Add posts from all branches in the home page - Modify the _main_content.html.erb file - Define instance variables inside the PagesController’s index action - Modify the no_posts_partial_path helper method to be more reusable - Add CSS to style the home page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -19,8 +19,8 @@ module PostsHelper
     current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
   end
 
-  def no_posts_partial_path
-    @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+  def no_posts_partial_path(posts)
+    posts.empty? ? 'posts/shared/no_posts' : 'shared/empty_partial'
   end
 
   def category_field_partial_path
