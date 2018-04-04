fa9253dbc2373069b9aa4688b09733d55f6accd5
kHigasa <kojihigasa@gmail.com>
2018-04-04 10:38:51 +0900

    Create a service object to extract logic from the get_posts method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,11 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
 
+  respond_to do |format|
+    format.html
+    format.js { render partial: 'posts/posts_pagination_page' }
+  end
+
   private
 
     def posts_for_branch(branch)
@@ -23,24 +28,10 @@ class PostsController < ApplicationController
     end
 
     def get_posts
-      branch = params[:action]
-      search = params[:search]
-      category = params[:category]
-
-      if category.blank? && search.blank?
-        posts = Post.by_branch(branch).all
-      elsif  category.blank? && search.present?
-        posts = Post.by_branch(branch).search(search)
-      elsif category.present? && search.blank?
-        posts = Post.by_category(branch, category)
-      elsif category.present? && search.present?
-        posts = Post.by_category(branch, category).search(search)
-      else
-      end
-    end
-
-    respond_to do |format|
-      format.html
-      format.js { render partial: 'posts/posts_pagination_page' }
+      PostsForBranchService.new({
+        search: params[:search],
+        category: params[:category],
+        branch: params[:action]
+      }).call
     end
 end
